bc27ef7836eaf3c9919244bfae5554feb7fd02ce
Andrés <alumno.254958@ies-azarquiel.es>
2025-04-27 21:14:54 +0200

    Updated settings.py

--- keepmesafe/settings.py
+++ keepmesafe/settings.py
@@ -10,6 +10,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=0)
+if os.environ.get('DEBUG') == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
