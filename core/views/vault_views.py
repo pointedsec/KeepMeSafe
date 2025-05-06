959f431187bec6c40881040697b679e3b9da7d52
Andrés <alumno.254958@ies-azarquiel.es>
2025-05-06 22:06:38 +0200

    Updated vault_views.py | Update vault name/password feature

--- core/views/vault_views.py
+++ core/views/vault_views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 import os
 from django.http import HttpResponseNotAllowed, HttpResponseServerError, JsonResponse
 import logging
 from core.models import Profile
 from cryptography.fernet import Fernet
+from core.utils.encrypted_actions import check_master_key, update_vault_encryption_key, gen_master_key
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -64,3 +65,39 @@ def delete_vault(request, profile_id):
         return JsonResponse({'message': 'Vault deleted successfully'}, status=200)
     else:
         return HttpResponseNotAllowed('Method not allowed')
+
+def edit_vault(request, profile_id):
+    profile = get_object_or_404(Profile, id=profile_id)
+    if request.method == 'POST':
+        new_name = request.POST.get('new_vault_name', '').strip()
+        current_password = request.POST.get('current_password', '')
+        new_password = request.POST.get('new_password', '')
+
+        # Check actual 'password'
+        if not check_master_key(profile, current_password):
+            return JsonResponse({'message': 'Password doesn\'t match with your actual password'}, status=500)
+
+        # Check if a vault exists with the new name
+        if new_name and Profile.objects.filter(name=new_name).exclude(id=profile.id).exists():
+            return JsonResponse({'message': 'A vault exists already with that name'}, status=500)
+
+        # Update name
+        if new_name:
+            profile.name = new_name
+
+        # To update the 'password', we have to decrypt the vault to encrypt it with the new vault key
+        if new_password:
+            encoded_key = request.session.get('vault_key')
+            cipher = Fernet(encoded_key.encode())
+            try:
+                update_vault_encryption_key(profile.vault_path.path, cipher, new_password)
+                profile.save()
+                fernet, encoded_key = gen_master_key(new_password)
+                request.session['vault_key'] = encoded_key
+                request.session['vault_name'] = profile.name.capitalize()
+            except Exception as e:
+                logger.error('Error updating encryption key for vault')
+                return JsonResponse({'message': 'An error ocurred while changing the vault name or key'}, status=500)
+
+        return JsonResponse({'message': 'Vault updated successfully'}, status=200)
+    return JsonResponse({'message': 'Method not allowed'}, status=500)
