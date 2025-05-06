266cb45697cc0505333f44a159600f73d6d38836
Andrés <alumno.254958@ies-azarquiel.es>
2025-05-06 21:07:34 +0200

    Updated urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 # keepmesafe/urls.py
 from django.urls import path
-from . import views
+from .views import profile_accessed, create_profile, profile_created, close_vault, delete_vault, admin_gate, login_profile
 
 urlpatterns = [
-    path('create_profile/', views.create_profile, name='create_profile'),
-    path('profile_created/<uuid:profile_id>/', views.profile_created, name='profile_created'),
-    path('', views.login_profile, name='login_profile_input'),
-    path('profile_accessed/<uuid:profile_id>/', views.profile_accessed, name='profile_accessed'),
-    path('vault/close/', views.close_vault, name='close_vault'),
-    path('vault/delete/<uuid:profile_id>/', views.delete_vault, name='delete_vault'),
-    path('admin-auth/', views.admin_gate, name='admin_gate'),
+    path('create_profile/', create_profile, name='create_profile'),
+    path('profile_created/<uuid:profile_id>/', profile_created, name='profile_created'),
+    path('', login_profile, name='login_profile_input'),
+    path('profile_accessed/<uuid:profile_id>/', profile_accessed, name='profile_accessed'),
+    path('vault/close/', close_vault, name='close_vault'),
+    path('vault/delete/<uuid:profile_id>/', delete_vault, name='delete_vault'),
+    path('admin-auth/', admin_gate, name='admin_gate'),
 ]
 
